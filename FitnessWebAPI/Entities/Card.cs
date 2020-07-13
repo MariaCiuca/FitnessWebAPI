e865177a7a32f2145ef1573cdf2bfd131fa31ff0
maria.ciuca2019@outlook.com <maria.ciuca2019@outlook.com>
2020-07-14 00:09:45 +0300

    added classes

---
+++ FitnessWebAPI/Entities/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessWebAPI.Entities
+{
+    public class Card
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+        [Required]
+        public Guid ValabilityID { get; set; }
+
+        [ForeignKey("ValabilityId")]
+        public virtual Valability Valability { get; set; }
+
+        public bool? Deleted { get; set; }
+    }
+
+   
+}
---
+++ FitnessWebAPI/Entities/Valability.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitnessWebAPI.Entities
+{
+    public class Valability
+    {
+        [Key]
+        public Guid ID { get; set; }
+
+
+        [Required]
+        public int Day { get; set; }
+
+        [Required]
+        public int Month { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        public bool? Deleted { get; set; }
+    }
+}
